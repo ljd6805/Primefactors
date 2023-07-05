a85ddb611e20f379e72a8be6f0182b3684784326
jdeok <ljd6805@gmail.com>
2023-07-05 15:13:18 +0900

    [Of1]34 step1 commit

---
+++ Project5/prime-factors.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using std::vector;
+
+class PrimeFactors
+{
+public:
+	vector<int> of(int number)
+	{
+		vector<int> prime_numbers = {};
+		return prime_numbers;
+	}
+};
--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
+#include "../Project5/prime-factors.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, Of1) {
+	PrimeFactors prime_factors;
+	std::vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factors.of(1));
 }
