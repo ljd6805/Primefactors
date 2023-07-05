0f9e0d1d612e0167f9cc4edcf3bb0bb46010b9b2
jdeok <ljd6805@gmail.com>
2023-07-05 15:25:56 +0900

    [Of3]42 step3 refactor commit

--- Project5/prime-factors.cpp
+++ Project5/prime-factors.cpp
@@ -8,10 +8,11 @@ public:
 	vector<int> of(int number)
 	{
 		vector<int> result = {};
-		if (number == 2)
+		if (number > 1)
 		{
-			result.push_back(2);
+			result.push_back(number);
 		}
+		
 		return result;
 	}
 };
--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -12,7 +12,13 @@ TEST_F(PrimeFixture, Of1) {
 	expected = {};
 	EXPECT_EQ(expected, prime_factors.of(1));
 }
+
 TEST_F(PrimeFixture, Of2) {
 	expected = {2};
 	EXPECT_EQ(expected, prime_factors.of(2));
+}
+
+TEST_F(PrimeFixture, Of3) {
+	expected = {3};
+	EXPECT_EQ(expected, prime_factors.of(3));
 }
