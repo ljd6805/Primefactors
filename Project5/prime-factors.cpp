79b5bf73fa95b524a6b810f37acc225a05c34e9c
jdeok <ljd6805@gmail.com>
2023-07-05 15:28:52 +0900

    [Of3]44 step4 green commit

--- Project5/prime-factors.cpp
+++ Project5/prime-factors.cpp
@@ -8,7 +8,12 @@ public:
 	vector<int> of(int number)
 	{
 		vector<int> result = {};
-		if (number > 1)
+		if(number==4)
+		{
+			result.push_back(2);
+			result.push_back(2);
+		}
+		else if (number > 1)
 		{
 			result.push_back(number);
 		}
